d31a23d864bf92be5edf4b7b8d52d75ef9491d17
Jeremias Santos Lima <69019443+JereLima@users.noreply.github.com>
2024-11-02 18:59:43 -0300

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,11 @@
+import React from "react";
 import { Stack } from "expo-router";
-import { View } from "react-native";
-import "react-native-reanimated";
 
 export default function RootLayout() {
   return (
-    <View className="flex-1 bg-fuchsia-400">
-      <Stack>
-        <Stack.Screen name="home" options={{ headerShown: false }} />
-        <Stack.Screen name="about" options={{ presentation: "modal" }} />
-      </Stack>
-    </View>
+    <Stack>
+      <Stack.Screen name="home" />
+      <Stack.Screen name="about" />
+    </Stack>
   );
 }
